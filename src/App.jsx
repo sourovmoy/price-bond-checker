272d13142b42742ce57ffbb836aab9090d07fecb
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-12-24 15:43:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import pdfToText from "react-pdftotext";
+
+function App() {
+  const [myText1, setMyTexts1] = useState([]);
+  const [myText2, setMyTexts2] = useState([]);
+  const [match, setMatch] = useState([]);
+  const handleFileChange1 = async (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+    if (file.type !== "application/pdf") {
+      alert("Please upload a PDF file");
+      return;
+    }
+
+    try {
+      const text = await pdfToText(file);
+      const result = text.match(/\d{7}/g);
+      setMyTexts1(result);
+    } catch (error) {
+      console.error("Failed to read PDF:", error);
+    }
+  };
+  const handleFileChange2 = async (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+    if (file.type !== "application/pdf") {
+      alert("Please upload a PDF file");
+      return;
+    }
+
+    try {
+      const text = await pdfToText(file);
+      const result = text.match(/\d{7}/g);
+      setMyTexts2(result);
+    } catch (error) {
+      console.error("Failed to read PDF:", error);
+    }
+  };
+  const fn = () => {
+    const match = myText1.filter((num) => myText2.includes(num));
+    setMyTexts1();
+    setMyTexts2();
+    setMatch(match);
+  };
+
+  return (
+    <div className=" container mx-auto px-10 mt-20">
+      <div className="flex justify-between">
+        <div>
+          <p className="mb-5 text-xl font-medium">Your pdf file</p>
+          <input
+            type="file"
+            accept="application/pdf"
+            className="file-input"
+            onChange={handleFileChange1}
+          />
+        </div>
+        <div>
+          <p className="mb-5 text-xl font-medium">Result pdf file</p>
+          <input
+            type="file"
+            accept="application/pdf"
+            className="file-input"
+            onChange={handleFileChange2}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div>
+          <button className="btn btn-primary" onClick={() => fn()}>
+            click
+          </button>
+        </div>
+      </div>
+      {match.length === 0 ? (
+        <p>No match found</p>
+      ) : (
+        match.map((r, i) => (
+          <p key={r} className="font-medium">
+            {i + 1}. Your wining numbers{" "}
+            <span className="text-green-400">{r}</span>
+          </p>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default App;
